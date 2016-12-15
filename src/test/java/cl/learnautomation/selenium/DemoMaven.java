80f71c402170170c216a485e4aaa444db6d26438
jorpalmauribe <jor.palmauribe@gmail.com>
2016-12-15 17:59:54 -0300

    se añade todo

---
+++ src/test/java/cl/learnautomation/selenium/DemoMaven.java
@@ -0,0 +1,17 @@
+package cl.learnautomation.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.*;
+
+public class DemoMaven {
+	
+	@Test
+	public void testHola()
+	{
+		System.out.println("Hola maven");
+		
+		
+	}
+
+}
---
+++ src/test/java/cl/learnautomation/selenium/DemoMaven2.java
@@ -0,0 +1,17 @@
+package cl.learnautomation.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.*;
+
+public class DemoMaven2 {
+	
+	@Test
+	public void testHola()
+	{
+		System.out.println("Hola maven 2");
+		
+		
+	}
+
+}
